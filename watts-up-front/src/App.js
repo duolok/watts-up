d56503bd1b5850a808e1762f1876941ffbb87257
duolok <dusan.lecic5@gmail.com>
2024-10-16 23:28:03 +0200

    add: starter style | useful css classes

--- watts-up-front/src/App.js
+++ watts-up-front/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ColorModeContext, useMode } from './theme.js';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [theme, colorMode]  = useMode();
+
+  return (<ColorModeContext.Provider value ={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
+
+    </ThemeProvider>
+  </ColorModeContext.Provider>
   );
-}
+};
 
 export default App;

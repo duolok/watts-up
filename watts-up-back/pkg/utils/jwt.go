f00f44447ebd02580ec149e66cfc052af890bb90
duolok <dusan.lecic5@gmail.com>
2024-11-03 16:10:02 +0100

    add: jwt token create and verify methods

---
+++ watts-up-back/pkg/utils/jwt.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+type Claims struct {
+	jwt.RegisteredClaims
+
+	Uid      uint `json:"uid"`
+	Username uint `json:"username"`
+}
+
+func GenerateToken(claims *Claims) string {
+    EnvConfig := configs.EnvConfig
+
+    claims.ExpiresAt = jwt.NewNumericDate(time.Now().Add(time.Minute * 60))
+
+    token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(EnvConfig.Jwt.Secret))
+    if err != nil {
+        panic(err)
+    }
+
+    return token
+}
+
+
+func JwtVerify(tokenStr string) (*Claims, error) {
+    EnvConfig := configs.EnvConfig
+    token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+        }
+        return []byte(EnvConfig.Jwt.Secret), nil
+    })
+
+    if !token.Valid || err != nil {
+        return nil, fmt.Errorf("Token invalid")
+    }
+
+    claims, ok := token.Claims.(*Claims)
+    if !ok {
+        return nil, err
+    }
+
+    if float64(claims.ExpiresAt.Unix()) < float64(time.Now().Unix()) {
+        return nil, fmt.Errorf("Token expired")
+    }
+
+    return claims, err
+}

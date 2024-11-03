cd29a94e4a4e2d76f6fb06403576d11bf7ad37ab
duolok <dusan.lecic5@gmail.com>
2024-11-03 15:10:44 +0100

    add: dummy public router

---
+++ watts-up-back/internal/router/public.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"github.com/gin-gonic-gin"
+    "gitlab.com/duolok/watts-up-back/watts-up/internal/controllers"
+)
+
+var publicController = new(controllers.PublicController)
+
+
+func LoadPublicRoutes(r *gin.Engine) *gin.RouterGroup {
+
+    public := r.Group("/public")
+    {
+        public.GET("/ping", publicController.Ping)
+    }
+
+    return public
+}

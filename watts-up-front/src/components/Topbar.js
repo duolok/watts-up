13d5f5c02f7adb9ad6a88aec8b1cec4ef98fac88
duolok <dusan.lecic5@gmail.com>
2024-11-04 15:01:24 +0100

    add: topbar component

---
+++ watts-up-front/src/components/Topbar.js
@@ -0,0 +1,51 @@
+import React, { useContext } from "react";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { ColorModeContext, tokens } from '../theme';
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box display="flex" alignItems="center">
+        <Typography
+          variant="h4" 
+          sx={{
+            color:
+              theme.palette.mode === "dark"
+                ? theme.palette.primary.main
+                : theme.palette.primary.main,
+            fontWeight: 100,
+            mr: 2, 
+          }}
+        >
+          WattsUp
+        </Typography>
+      </Box>
+
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;

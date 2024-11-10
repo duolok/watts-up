1641f6ae2e08848d3bceb03fed640a54c3d89516
duolok <dusan.lecic5@gmail.com>
2024-11-10 21:07:27 +0100

    add: register property route

--- watts-up-front/src/App.js
+++ watts-up-front/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import AssetsPage from './pages/AssetsPage.js'
+import RegisterPropertyPage from './pages/user/RegisterPropertyPage'
 
 const App = () => {
   const [theme, colorMode]  = useMode();
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/assets" element={<AssetsPage />} />
+          <Route path="/register-property" element={<RegisterPropertyPage />} />
         </Routes>
       </Router>
 
